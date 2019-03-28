61eadc882fd2260b4d579ffe30807c6d3befb1ae
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2019-03-27 22:17:18 -0400

    Add browser-sync watch

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,21 @@ const browserSync = require('browser-sync').create();
 function style() {
     return gulp.src('./app/styles/scss/**/*.scss')
             .pipe(sass())
-            .pipe(gulp.dest('./app/styles/css'));
+            .pipe(gulp.dest('./app/styles/css'))
+            .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    });
+
+    gulp.watch('./app/styles/scss/**/*.scss', style);
+    gulp.watch('./app/*.html').on('change', browserSync.reload);
+    gulp.watch('./app/scripts/js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
+exports.watch = watch;
