1834573892f06406fd1117bae02b1294dc248189
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2019-04-13 23:01:19 -0400

    Add gulp 'build' task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { gulp, series } = require('gulp');
+const gulp = require('gulp');
 const del = require('del');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
@@ -6,8 +6,7 @@ const browserSync = require('browser-sync').create();
 function style() {
     return gulp.src('./app/styles/scss/**/*.scss')
             .pipe(sass())
-            .pipe(gulp.dest('./app/styles/css'))
-            .pipe(browserSync.stream());
+            .pipe(gulp.dest('./app/styles/css'));
 }
 
 function watch() {
@@ -17,7 +16,9 @@ function watch() {
         }
     });
 
-    gulp.watch('./app/styles/scss/**/*.scss', style);
+    const browserSyncStyle = () => style().pipe(browserSync.stream());
+
+    gulp.watch('./app/styles/scss/**/*.scss', browserSyncStyle);
     gulp.watch('./app/*.html').on('change', browserSync.reload);
     gulp.watch('./app/scripts/js/**/*.js').on('change', browserSync.reload);
 }
@@ -28,12 +29,17 @@ function cleanDist(done) {
 }
 
 function createDist(done) {
-
-
+    gulp.src('./app/index.html').pipe(gulp.dest('./dist'));
+    gulp.src('./app/styles/**/*.css').pipe(gulp.dest('./dist/styles'));
     done();
 }
 
 exports.style = style;
 exports.watch = watch;
 exports.clean = cleanDist;
-exports.build = series(cleanDist, createDist);
+
+exports.build = gulp.series(
+    cleanDist,
+    style,
+    createDist,
+    );
