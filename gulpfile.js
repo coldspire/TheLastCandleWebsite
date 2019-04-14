6fff41e33ca90f4245fdc934eb4dc06031559315
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2019-04-13 23:05:47 -0400

    Change paths to refer to new 'path' object values

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,39 @@ const del = require('del');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
+const path = {
+    app: 'app',
+    dist: 'dist',
+};
+
 function style() {
-    return gulp.src('./app/styles/scss/**/*.scss')
+    return gulp.src(`./${path.app}/styles/scss/**/*.scss`)
             .pipe(sass())
-            .pipe(gulp.dest('./app/styles/css'));
+            .pipe(gulp.dest(`./${path.app}/styles/css`));
 }
 
 function watch() {
     browserSync.init({
         server: {
-            baseDir: 'app'
+            baseDir: path.app
         }
     });
 
     const browserSyncStyle = () => style().pipe(browserSync.stream());
 
-    gulp.watch('./app/styles/scss/**/*.scss', browserSyncStyle);
-    gulp.watch('./app/*.html').on('change', browserSync.reload);
-    gulp.watch('./app/scripts/js/**/*.js').on('change', browserSync.reload);
+    gulp.watch(`./${path.app}/styles/scss/**/*.scss`, browserSyncStyle);
+    gulp.watch(`./${path.app}/*.html`).on('change', browserSync.reload);
+    gulp.watch(`./${path.app}/scripts/js/**/*.js`).on('change', browserSync.reload);
 }
 
 function cleanDist(done) {
-    del.sync('dist');
+    del.sync(path.dist);
     done();
 }
 
 function createDist(done) {
-    gulp.src('./app/index.html').pipe(gulp.dest('./dist'));
-    gulp.src('./app/styles/**/*.css').pipe(gulp.dest('./dist/styles'));
+    gulp.src(`./${path.app}/index.html`).pipe(gulp.dest(`./${path.dist}`));
+    gulp.src(`./${path.app}/styles/**/*.css`).pipe(gulp.dest(`./${path.dist}/styles`));
     done();
 }
 
